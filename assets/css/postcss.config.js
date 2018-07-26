4ac9f71481754bdda864810db6ed44ed7c74c952
budparr <budparr@gmail.com>
2018-07-25 20:25:26 -0400

    Add fontface to purge

--- assets/css/postcss.config.js
+++ assets/css/postcss.config.js
@@ -1,12 +1,11 @@
-
 class TailwindExtractor {
 	static extract(content) {
 		return content.match(/[A-z0-9-:\/]+/g)
 	}
 }
 
-module.exports = {  
-  plugins: [
+module.exports = {    
+  plugins: [        
     require('postcss-import')({
       path: ["assets/css"],
     }), 
@@ -17,9 +16,10 @@ module.exports = {
       {
         extractor: TailwindExtractor,
         extensions: ['html']
-      }],
+      }], 
+      fontFace: true,
       whitelist: ['class1', 'class2']
-    }),
+    }),    
     require('autoprefixer')({
       grid: true,
       browsers: ['>1%']
