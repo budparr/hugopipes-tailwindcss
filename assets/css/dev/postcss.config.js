2e5e0eeadfe797c41d4a931b489e81d3425c196b
budparr <budparr@gmail.com>
2018-07-30 13:54:58 -0400

    Add dev only config to exclude PurgeCSS while working locally

---
+++ assets/css/dev/postcss.config.js
@@ -0,0 +1,17 @@
+class TailwindExtractor {
+	static extract(content) {
+		return content.match(/[A-z0-9-:\/]+/g)
+	}
+}
+
+module.exports = {    
+  plugins: [        
+    require('postcss-import')({
+      path: ["assets/css"],
+    }), 
+    require('tailwindcss')('./assets/css/tailwind.js'),    
+    require('autoprefixer')({      
+      browsers: ['>1%']
+    }),    
+  ]
+}
